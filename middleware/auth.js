a4de495a7c32edaff52b7361f8eb2d2eee204854
Somsubhra97 <somsubhrablasterjaxxmitra@gmail.com>
2021-03-13 11:28:13 +0530

    init

---
+++ middleware/auth.js
@@ -0,0 +1,54 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./async');
+const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/User');
+
+// Protect routes
+exports.protect = (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')||
+    req.headers('x-auth-token')||
+    req.headers['authorization']
+  ) {
+     token = req.headers.authorization.split(' ')[1];
+   //token=req.headers['authorization'].split(' ')[1];
+  }
+if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
+  } catch (err) {
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
+  }
+}
+
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
+
